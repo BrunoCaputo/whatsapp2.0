eba1ed4dbd4e4a54572eeb9a31741a37e6963a5b
BrunoCaputo <brunopereirac15@gmail.com>
2022-02-22 20:38:19 -0300

    feat: Added sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import { Avatar, IconButton, Button } from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Sidebar() {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in Chats" />
+      </Search>
+
+      <SidebarButton>Start a New Chat</SidebarButton>
+
+      {/* List of chats */}
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  border-radius: 2px;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+    color: black;
+  }
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
--- pages/chat.js
+++
@@ -1,9 +0,0 @@
-function chat() {
-  return (
-    <div>
-      <h1>This is the chat page</h1>
-    </div>
-  );
-}
-
-export default chat;
