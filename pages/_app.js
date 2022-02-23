19f795daf513969e04b1fae536c34979adcbbd6b
BrunoCaputo <brunopereirac15@gmail.com>
2022-02-22 23:07:29 -0300

    feat: Added login page and google authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import Login from "./login";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user] = useAuthState(auth);
+
+  if (!user) return <Login />;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/login.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import Head from "next/head";
+import { Button } from "@mui/material";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebase";
+
+export function Login() {
+  const whatsappLogo =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2042px-WhatsApp.svg.png";
+
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch(alert);
+  };
+
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <LoginContainer>
+        <Logo src={whatsappLogo} />
+        <Button onClick={signIn} variant="outlined">
+          Sign in with Google
+        </Button>
+      </LoginContainer>
+    </Container>
+  );
+}
+
+export default Login;
+
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: whitesmoke;
+`;
+
+const LoginContainer = styled.div`
+  padding: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
+`;
+
+const Logo = styled.img`
+  height: 200px;
+  width: 200px;
+  margin-bottom: 50px;
+`;
