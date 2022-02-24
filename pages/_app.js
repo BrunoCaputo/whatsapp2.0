a5733f8c1ca8556b80bcd1bcd110cbedf8dd6c4d
BrunoCaputo <brunopereirac15@gmail.com>
2022-02-23 23:32:55 -0300

    chore: Store user data into Firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
 import "../styles/globals.css";
+
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
+import { useEffect } from "react";
+import { doc, setDoc, Timestamp } from "firebase/firestore";
+
 import Login from "./login";
 import Loading from "../components/Loading";
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      const document = doc(db, `users/${user.uid}`);
+      setDoc(
+        document,
+        {
+          email: user.email,
+          lastSeen: Timestamp.now(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
 
   if (loading) return <Loading />;
   if (!user) return <Login />;
