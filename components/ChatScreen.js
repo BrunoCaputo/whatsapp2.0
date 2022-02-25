d37069dac645ea4d350496d0a62f7c903cefaa12
BrunoCaputo <brunopereirac15@gmail.com>
2022-02-24 23:19:32 -0300

    feat: Added chat (input field, show messages and chat header)

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,110 @@
 import styled from "styled-components";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { auth, db } from "../firebase";
+import { useRouter } from "next/router";
+import { Avatar, IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
+import {
+  collection,
+  doc,
+  orderBy,
+  query,
+  setDoc,
+  Timestamp,
+  addDoc,
+} from "firebase/firestore";
+import Message from "./Message";
+import { useState } from "react";
 
-function Chatscreen() {
+function Chatscreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const [input, setInput] = useState("");
+  const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    query(
+      collection(db, `chats/${router.query.id}/messages`),
+      orderBy("timestamp", "asc")
+    )
+  );
+
+  const showMessages = () => {
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    const docRef = doc(db, `chats/${user.uid}`);
+    setDoc(
+      docRef,
+      {
+        lastSeen: Timestamp.now(),
+      },
+      { merge: true }
+    );
+
+    const colRef = collection(db, `chats/${router.query.id}/messages`);
+    addDoc(colRef, {
+      timestamp: Timestamp.now(),
+      message: input,
+      user: user.email,
+      photoURL: user.photoURL,
+    });
+
+    setInput("");
+  };
+
   return (
     <Container>
-      <h1>This is a chat</h1>
+      <Header>
+        <Avatar />
+
+        <HeaderInformation>
+          <h3>Rec Email</h3>
+          <p>Last seen ...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+
+      <MessageContainer>
+        {showMessages()}
+        <EndOfMessage />
+      </MessageContainer>
+
+      <InputContainer>
+        <IconButton>
+          <InsertEmoticonIcon />
+        </IconButton>
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button hidden disabled={!input} type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+        <IconButton>
+          <MicIcon />
+        </IconButton>
+      </InputContainer>
     </Container>
   );
 }
@@ -11,3 +112,60 @@ function Chatscreen() {
 export default Chatscreen;
 
 const Container = styled.div``;
+
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div`
+  padding: 30px;
+  background-color: #e5ded8;
+  min-height: 90vh;
+`;
+
+const EndOfMessage = styled.div``;
+
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: white;
+  z-index: 100;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  outline: 0;
+  border: none;
+  border-radius: 10px;
+  background-color: whitesmoke;
+  padding: 20px;
+  margin-left: 15px;
+  margin-right: 15px;
+`;
