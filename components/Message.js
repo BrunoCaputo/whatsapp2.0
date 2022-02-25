5724ccd0a21f529dd39da3e65749369284d3a99f
BrunoCaputo <brunopereirac15@gmail.com>
2022-02-25 18:48:03 -0300

    feat: Build the message component

--- components/Message.js
+++ components/Message.js
@@ -1,9 +1,21 @@
 import styled from "styled-components";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import moment from "moment";
 
 function Message({ user, message }) {
+  const [userLoggedIn] = useAuthState(auth);
+
+  const TypeOfMessage = user === userLoggedIn.email ? Sender : Receiver;
+
   return (
     <Container>
-      <p>{message.message}</p>
+      <TypeOfMessage>
+        {message.message}
+        <Timestamp>
+          {message.timestamp ? moment(message.timestamp).format("LT") : "..."}
+        </Timestamp>
+      </TypeOfMessage>
     </Container>
   );
 }
@@ -11,3 +23,34 @@ function Message({ user, message }) {
 export default Message;
 
 const Container = styled.div``;
+
+const MessageElement = styled.p`
+  width: fit-content;
+  padding: 15px;
+  border-radius: 8px;
+  margin: 10px;
+  min-width: 60px;
+  padding-bottom: 26px;
+  position: relative;
+  text-align: right;
+`;
+
+const Sender = styled(MessageElement)`
+  margin-left: auto;
+  background-color: #dcf8c6;
+`;
+
+const Receiver = styled(MessageElement)`
+  text-align: left;
+  background-color: whitesmoke;
+`;
+
+const Timestamp = styled.span`
+  color: gray;
+  padding: 10px;
+  font-size: 9px;
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 0;
+`;
